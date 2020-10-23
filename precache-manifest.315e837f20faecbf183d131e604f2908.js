6bae838b9851419d747a01e3b48d46ffa25ccd9e
Sabri SELMI <sabri.selmi1992@gmail.com>
2020-10-23 15:12:52 +0100

    Updates

---
+++ precache-manifest.315e837f20faecbf183d131e604f2908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e45ed5d277e4bde85406866c5e74aec",
+    "url": "/Alekhbariya/index.html"
+  },
+  {
+    "revision": "109e588a109d546e5f4c",
+    "url": "/Alekhbariya/static/css/2.94491f11.chunk.css"
+  },
+  {
+    "revision": "ec72ae30c259ec4bcf75",
+    "url": "/Alekhbariya/static/css/main.014bdffe.chunk.css"
+  },
+  {
+    "revision": "109e588a109d546e5f4c",
+    "url": "/Alekhbariya/static/js/2.8422ae54.chunk.js"
+  },
+  {
+    "revision": "4d75f6136d35ed54ae1c8039011a0d55",
+    "url": "/Alekhbariya/static/js/2.8422ae54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec72ae30c259ec4bcf75",
+    "url": "/Alekhbariya/static/js/main.707a2673.chunk.js"
+  },
+  {
+    "revision": "62a9dab807d7736d587d",
+    "url": "/Alekhbariya/static/js/runtime-main.90e6ba4d.js"
+  }
+]);
