cdc6be69d4fe16a969ab6f6648817056dc85ee4d
Sabri SELMI <sabri.selmi1992@gmail.com>
2020-10-23 21:07:36 +0100

    fix responsive caroussel

--- src/pages/HomePage/SectionThree/index.jsx
+++ src/pages/HomePage/SectionThree/index.jsx
@@ -11,10 +11,28 @@ const SectionThree = () => {
                     className={"owl-sport owl-carousel owl-theme owl-rtl owl-loaded owl-drag"} 
                     itemsNumber={4} 
                     margin={10}
+                    dots={false}
                     autoplay={false}
                     loop={false}
                     items={data}
                     classTitle="mediumTitle"
+                    responsive={{
+                        0:{
+                            items : 1
+                        },
+                        480:{
+                            items:2
+                        },
+                        768:{
+                            items:3
+                        },
+                        992:{
+                            items:4
+                        },
+                        1600:{
+                            items:5
+                        }
+                    }}
                 />
         </section>
      );
